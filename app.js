004b5cce27567967c6c5c63719a6f73754bc5fa7
Abdul Lahad <lahad@example.com>
2025-12-15 10:30:55 +0500

    add(cors package)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { PORT } from './config/env.js';
 
@@ -16,6 +17,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors());
+
 
 app.use(arcjetMiddleware);
 
